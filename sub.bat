0460ca3adb13f605831d047c5f362811263da751
Zhaopinglu <zhaopinglu77@gmail.com>
2024-03-06 10:16:45 +0800

    rust workflow

--- sub.bat
+++
@@ -1,1 +0,0 @@
-curl -i -X POST -d "email=aaa5@bbb.com&username=lzp" http://127.0.0.1:8000/subscriptions
---
+++ test.bat
@@ -0,0 +1,2 @@
+curl -i -X POST -d "email=aaa5@bbb.com&username=lzp" http://127.0.0.1:8000/subscriptions
+curl -v http://127.0.0.1:8000/health_check
