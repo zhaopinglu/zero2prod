b369256f1f5864840a728f22d004ffb213d916d1
Zhaopinglu <zhaopinglu77@gmail.com>
2024-03-07 14:33:19 +0800

    Introduce PgConnectionOptions

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,26 +1,32 @@
 // use config::Environment;
 use secrecy::{ExposeSecret, Secret};
 // use tracing_subscriber::fmt::format;
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::PgConnectOptions;
 
 #[derive(serde::Deserialize)]
-pub struct Settings {
-    pub database: DatabaseSettings,
-    pub application: ApplicationSettings,
+pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String,
 }
+
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
 }
 
 #[derive(serde::Deserialize)]
-pub struct ApplicationSettings {
-    pub port: u16,
-    pub host: String,
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application: ApplicationSettings,
 }
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let base_path = std::env::current_dir()
         .expect("Failed to determine the current directory");
@@ -41,31 +47,30 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(config::File::from(
             configuration_directory.join(environment_filename),
         ))
+        // Add in settings from environment variables (with a prefix of APP and
+        // '__' as separator)
+        // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
         .build()?;
     // Try to convert the configuration values it read into
     // our Settings type
     settings.try_deserialize::<Settings>()
 }
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port,
-            self.database_name
-        ))
+    pub fn with_db(&self) -> PgConnectOptions {
+        self.without_db().database(&self.database_name)
     }
 
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username,
-            self.password.expose_secret(),
-            self.host,
-            self.port
-        ))
+    pub fn without_db(&self) -> PgConnectOptions {
+        PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+// use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
+// use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,14 +13,14 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration =
         get_configuration().expect("Failed to read configuration.");
-    let pool = PgPool::connect_lazy(
-        &configuration.database.connection_string().expose_secret(),
-    )
-    .expect("Failed to create Postgres connection pool....");
+
+    let connection_pool = PgPoolOptions::new()
+        .connect_lazy_with(configuration.database.with_db());
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address).expect("Failed to bind");
-    run(listener, pool)?.await
+    run(listener, connection_pool)?.await
 }
